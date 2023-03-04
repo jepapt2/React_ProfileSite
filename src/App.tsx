f813dd56879c0f3fb5f68946418fb443acde5a31
jepapt2 <j.webprogram@gmail.com>
2023-03-04 23:51:13 +0900

    add_react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import AppContainer from './styles/AppContainer'
 import { ThemeProvider } from 'styled-components'
 import Theme from './styles/theme'
-import Card from './components/ui/Card'
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
-      <AppContainer>
-        <Card>aa</Card>
-      </AppContainer>
+      <AppContainer></AppContainer>
     </ThemeProvider>
   )
 }
