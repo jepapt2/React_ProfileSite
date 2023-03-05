c81c32844f55d7d1bfce927e5bec951175d088ef
jepapt2 <j.webprogram@gmail.com>
2023-03-05 21:36:56 +0900

    react-hook-form_add

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,26 @@
+import { useForm } from 'react-hook-form'
 import Card from '../components/ui/Card'
 import Head from '../components/ui/text/Head'
 
+type AuthForm = {
+  email: string
+  password: string
+}
+
 const Home: React.FC = () => {
+  const { register, handleSubmit } = useForm<AuthForm>()
+  const onSubmit = handleSubmit((data) => console.log(data))
   return (
     <>
       <Card>
         <Head>Profile</Head>
+        <form>
+          <input {...register('email')} type="email" />
+          <input {...register('password')} type="password" />
+          <button type="submit" onClick={onSubmit}>
+            Submit
+          </button>
+        </form>
       </Card>
     </>
   )
